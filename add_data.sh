2d3f39a2a156fc4354155548c0bcfc1f1e8b55e5
adlantz <asher.lantz96@gmail.com>
2025-05-06 23:35:37 -0700

    scripts output to text file

--- add_data.sh
+++ add_data.sh
@@ -7,11 +7,13 @@ set -e
 # source /path/to/venv/bin/activate
 
 # Run Python modules in order
-python -m scripts.db_fillers.0_add_instances
-python -m scripts.db_fillers.1_update_degeneracy_col
-python -m scripts.db_fillers.2_update_reduced_gs_hd
-python -m scripts.db_fillers.3_update_overlap_dist
-python -m scripts.db_fillers.4_update_post_anneal_gs_probs
-python -m scripts.db_fillers.5_update_post_anneal_overlap
-python -m scripts.db_fillers.7_update_qfi
-python -m scripts.db_fillers.9_full_gs_mq
+{
+    python -m scripts.db_fillers.0_add_instances
+    python -m scripts.db_fillers.1_update_degeneracy_col
+    python -m scripts.db_fillers.2_update_reduced_gs_hd
+    python -m scripts.db_fillers.3_update_overlap_dist
+    python -m scripts.db_fillers.4_update_post_anneal_gs_probs
+    python -m scripts.db_fillers.5_update_post_anneal_overlap
+    python -m scripts.db_fillers.7_update_qfi
+    python -m scripts.db_fillers.9_full_gs_mq
+} > N16logs.txt 2>&1
