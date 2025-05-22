f2f2eb1ab70cd58abb7952b0cb79d09ede1fc8df
adlantz <asher.lantz96@gmail.com>
2025-05-21 20:15:27 -0700

    recent runs

--- scripts/db_fillers/large_n/0_find_ground_states.py
+++ scripts/db_fillers/large_n/0_find_ground_states.py
@@ -6,11 +6,11 @@ import numpy as np
 from tqdm import tqdm
 
 
-N = 30
+N = 35
 
 with data_service.get_session() as session:
     instances = []
-    for seed in tqdm(range(0, 100)):
+    for seed in tqdm(range(0, 10)):
         gs, _ = large_n_service.generate_ground_states_bm(N, seed)
         instances.append(
             LargeN(size=N, seed=seed, ground_states=gs, degeneracy=len(gs))
